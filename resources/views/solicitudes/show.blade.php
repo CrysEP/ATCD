539cdc16755aa39548444a7b383c1c8b92e49b28
CCEP <crystalcactusep@gmail.com>
2026-02-10 11:02:25 -0400

    10 02 2026 La linea fastasma

--- resources/views/solicitudes/show.blade.php
+++ resources/views/solicitudes/show.blade.php
@@ -385,6 +385,8 @@
         </div>
     </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    
     {{-- NOTA IMPORTANTE: Se eliminó la importación manual de Bootstrap Bundle aquí porque causaba conflicto con la del Layout --}}
     
     <script>
